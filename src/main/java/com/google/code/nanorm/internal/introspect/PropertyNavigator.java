61197059d4543510269ac89df1869d68cae722d3
Ivan Dubrov <dubrov.ivan@gmail.com>
2008-06-21 12:30:07 +0000

    implemented property navigator
    git-svn-id: https://nanorm.googlecode.com/svn/trunk@24 a420aabd-eb4f-0410-91c9-6bc514024ae0

---
+++ src/main/java/com/google/code/nanorm/internal/introspect/PropertyNavigator.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (C) 2008 Ivan S. Dubrov
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.code.nanorm.internal.introspect;
+
+/**
+ * 
+ * @author Ivan Dubrov
+ * @version 1.0 21.06.2008
+ */
+public final class PropertyNavigator {
+
+    public static int INDEX = 1;
+
+    public static int PROPERTY = 2;
+    
+    private int token;
+
+    private int index = -1;
+
+    private String property;
+
+    private int pos;
+    
+    private String path;
+    
+    public PropertyNavigator(String path) {
+        this.path = path;
+    }
+
+    public final int getIndex() {
+        return index;
+    }
+    
+    public final String getProperty() {
+        return property;
+    }
+    
+    public final boolean isLast() {
+        return pos == path.length();
+    }
+    
+    public final int next() {
+        char c = path.charAt(pos);
+        if(c == '[') {
+            index = parseIndex();
+            property = null;
+            return INDEX;
+        } else if(Character.isJavaIdentifierStart(c)) {
+            property = parseProperty();
+            index = -1;
+            return PROPERTY;
+        }
+        throw unexpected();
+    }
+    
+    private int parseIndex() {
+        // Skip '['
+        pos++;
+        int start = pos;
+        while(pos < path.length()) {
+            char c = path.charAt(pos);
+            if(c == ']') {
+                int ind = Integer.parseInt(path.substring(start, pos));
+                
+                // Skip ']'
+                pos++;
+                
+                // Skip '.' after indexing operator
+                if(pos < path.length()) {
+                    if(path.charAt(pos) != '.') {
+                        throw unexpected();
+                    }
+                    pos++;
+                }
+                return ind;
+            }
+            if(!Character.isDigit(c)) {
+                // Invalid property path
+                break;
+            }
+            pos++;
+        }
+        throw unexpected();
+    }
+    
+    private String parseProperty() {
+        int start = pos;
+        while(pos < path.length()) {
+            char c = path.charAt(pos);
+            if(c == '.' || c == '[') {
+                String prop = path.substring(start, pos);
+                
+                // Skip '.'
+                if(c == '.') {
+                    pos++;
+                }
+                return prop;
+            }
+            if(!Character.isJavaIdentifierPart(c)) {
+                throw unexpected();
+            }
+            pos++;
+        }
+        // Return value up to the end
+        return path.substring(start);
+    }
+    
+    private IllegalArgumentException unexpected() {
+        return new IllegalArgumentException("Unexpected character at position " + pos + " in property path '" + path + '\'');
+    }
+}
---
+++ src/test/java/com/google/code/nanorm/test/TestPropNavigator.java
@@ -0,0 +1,202 @@
+/**
+ * Copyright (C) 2008 Ivan S. Dubrov
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.code.nanorm.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.code.nanorm.internal.introspect.PropertyNavigator;
+import static com.google.code.nanorm.internal.introspect.PropertyNavigator.INDEX;
+import static com.google.code.nanorm.internal.introspect.PropertyNavigator.PROPERTY;
+
+/**
+ *
+ * @author Ivan Dubrov
+ * @version 1.0 21.06.2008
+ */
+public class TestPropNavigator {
+
+    @Test
+    public void testPath1() {
+        PropertyNavigator nav = new PropertyNavigator("prop1.prop2.prop3");
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop1", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop2", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop3", nav.getProperty());
+        
+        Assert.assertTrue(nav.isLast());
+    }
+    
+    @Test
+    public void testPath2() {
+        PropertyNavigator nav = new PropertyNavigator("prop1.prop2[5].prop3");
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop1", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop2", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(INDEX, nav.next());
+        Assert.assertEquals(5, nav.getIndex());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop3", nav.getProperty());
+        
+        Assert.assertTrue(nav.isLast());
+    }
+    
+    @Test
+    public void testPath3() {
+        PropertyNavigator nav = new PropertyNavigator("prop1.prop2.prop3[7]");
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop1", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop2", nav.getProperty());
+        
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop3", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(INDEX, nav.next());
+        Assert.assertEquals(7, nav.getIndex());
+        
+        Assert.assertTrue(nav.isLast());
+    }
+    
+    @Test
+    public void testPath4() {
+        PropertyNavigator nav = new PropertyNavigator("[2].prop1.prop2.prop3");
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(INDEX, nav.next());
+        Assert.assertEquals(2, nav.getIndex());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop1", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop2", nav.getProperty());
+        
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop3", nav.getProperty());
+        
+        Assert.assertTrue(nav.isLast());
+    }
+    
+    @Test
+    public void testPath5() {
+        PropertyNavigator nav = new PropertyNavigator("prop1.prop2[5a].prop3");
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop1", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop2", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        try {
+            nav.next();
+            Assert.fail();
+        } catch(IllegalArgumentException e) {
+            // Nothing.
+        }
+    }
+    
+    @Test
+    public void testPath6() {
+        PropertyNavigator nav = new PropertyNavigator("prop1.prop2[5]b.prop3");
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop1", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop2", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        try {
+            nav.next();
+            Assert.fail();
+        } catch(IllegalArgumentException e) {
+            // Nothing.
+        }
+    }
+    
+    @Test
+    public void testPath7() {
+        PropertyNavigator nav = new PropertyNavigator("prop1.prop2]5].prop3");
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop1", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        try {
+            nav.next();
+            Assert.fail();
+        } catch(IllegalArgumentException e) {
+            // Nothing.
+        }
+    }
+    
+    @Test
+    public void testPath8() {
+        PropertyNavigator nav = new PropertyNavigator("prop1.prop2.^prop3");
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop1", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        Assert.assertEquals(PROPERTY, nav.next());
+        Assert.assertEquals("prop2", nav.getProperty());
+        
+        Assert.assertFalse(nav.isLast());
+        try {
+            nav.next();
+            Assert.fail();
+        } catch(IllegalArgumentException e) {
+            // Nothing.
+        }
+    }
+}
