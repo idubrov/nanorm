fd7d9d5a0e937500b4ccb2917daa615aa774c381
Ivan Dubrov <dubrov.ivan@gmail.com>
2008-07-30 15:53:44 +0000

    elaborating the guide
    git-svn-id: https://nanorm.googlecode.com/svn/trunk@139 a420aabd-eb4f-0410-91c9-6bc514024ae0

--- src/docs/build.sh
+++ src/docs/build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-xsltproc /usr/share/sgml/docbook/xsl-stylesheets/fo/docbook.xsl Guide.xml > Guide.fo
-fop -fo Guide.fo -pdf Guide.pdf
-rm Guide.fo
+#xsltproc /usr/share/sgml/docbook/xsl-stylesheets/fo/docbook.xsl Guide.xml > Guide.fo
+#fop -fo Guide.fo -pdf Guide.pdf
+#rm Guide.fo
+
+OPTS="$OPTS -P latex.unicode.use=1"
+OPTS="$OPTS -P doc.collab.show=0"
+OPTS="$OPTS -P latex.output.revhistory=0"
+OPTS="$OPTS -P latex.class.book=report"
+OPTS="$OPTS -P latex.hyperparam=linktocpage,colorlinks,citecolor=blue,pdfstartview=FitH"
+
+export XML_CATALOG_FILES="file:///usr/share/sgml/docbook/xml-dtd-4.5/catalog.xml"
+export  XML_DEBUG_CATALOG=1
+#dblatex -t tex $OPTS Guide.xml
+dblatex $OPTS Guide.xml
