b1f087acdc7cdccc7b99d6d6b37b9a440096f1d0
Ivan Dubrov <dubrov.ivan@gmail.com>
2008-07-31 04:59:58 +0000

    elaborating the guide
    git-svn-id: https://nanorm.googlecode.com/svn/trunk@140 a420aabd-eb4f-0410-91c9-6bc514024ae0

--- src/docs/build.sh
+++ src/docs/build.sh
@@ -8,8 +8,11 @@ OPTS="$OPTS -P doc.collab.show=0"
 OPTS="$OPTS -P latex.output.revhistory=0"
 OPTS="$OPTS -P latex.class.book=report"
 OPTS="$OPTS -P latex.hyperparam=linktocpage,colorlinks,citecolor=blue,pdfstartview=FitH"
+OPTS="$OPTS -P table.default.position=[hp]"
+OPTS="$OPTS -P table.in.float=0"
+OPTS="$OPTS -P table.title.top=1"
 
-export XML_CATALOG_FILES="file:///usr/share/sgml/docbook/xml-dtd-4.5/catalog.xml"
-export  XML_DEBUG_CATALOG=1
-#dblatex -t tex $OPTS Guide.xml
+#export XML_CATALOG_FILES="file:///usr/share/sgml/docbook/xml-dtd-4.5/catalog.xml"
+#export  XML_DEBUG_CATALOG=1
+dblatex -t tex $OPTS Guide.xml
 dblatex $OPTS Guide.xml
